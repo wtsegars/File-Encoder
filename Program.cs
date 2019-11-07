cc8a92dfe2015a9ed9a08434d17fb2f9dc9eb6b1
Darkangel <wtsegars@mac.com>
2019-11-06 21:22:35 -0500

    Added basic file structure

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace File_Encoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
